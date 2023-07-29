47e3201b15e6d7d16a0aeb0dbab2acce868f95ea
Sefa Başboğa <sefabsbgaaa@gmail.com>
2023-07-29 16:06:36 +0300

    Add project files.

---
+++ Casgem_Dapper/DAL/Headings.cs
@@ -0,0 +1,9 @@
+﻿namespace Casgem_Dapper.DAL
+{
+    public class Headings
+    {
+        public int HeadingID { get; set; }
+        public string HeadingName { get; set; }
+        public bool HeadingStatus { get; set; }
+    }
+}
